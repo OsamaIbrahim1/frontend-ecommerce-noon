b10f9ede2c9c549b9955483a8072b15ba2fe1d83
OsamaIbrahim1 <oi5921435@gmial.com>
2024-10-23 03:33:07 +0300

    feat: orderdetails

--- src/Components/Order/Order.jsx
+++ src/Components/Order/Order.jsx
@@ -24,31 +24,34 @@ export default function Order() {
             <title>Your Orders</title>
         </Helmet>
         <div className='row'>
-            <h1>Orders : </h1>
-            <Link to={'/'}>
-                {orders?.map((order) => <div key={order._id} className='order cursor-pointer px-2 py-3  d-flex'>
-                    <div className='col-md-4 mt-4'>
-                        {order.orderItems.map((item) => <>
-                            <h4 className='h6 fw-bolder'>title : {item.title}</h4>
-                            <h4 className='h6 fw-bolder'>quantity : {item.quantity}</h4>
+            {<>
+                <h1>Orders : </h1>
+                <Link to={'/'}>
+                    orders?.map((order) => <div key={order._id} className='order cursor-pointer px-2 py-3  d-flex'>
+                        <div className='col-md-4 mt-4'>
+                            {order.orderItems.map((item) => <>
+                                <h4 className='h6 fw-bolder'>title : {item.title}</h4>
+                                <h4 className='h6 fw-bolder'>quantity : {item.quantity}</h4>
 
-                        </>
+                            </>
 
-                        )}
+                            )}
+                        </div>
+                        <div className='col-md-4'>
+                            <h4 className='h4 text-center'>{order.orderStatus}</h4>
+                        </div>
+                        <div className='col-md-4 mt-4 '>
+                            <h4 className='h6 fw-bolder'>payment method : {order.paymentMethod}</h4>
+                            <h4 className='h6 fw-bolder'>is paid: {`${order.isPaid}`}</h4>
+                            <h4 className='h6 fw-bolder'>is delivered: {`${order.isDelivered}`}</h4>
+                            <h4 className='h6 fw-bolder'>total price: {order.totalPrice}</h4>
+                            <button className='btn bg-outline-main start-0'>more detailes</button>
+                        </div>
                     </div>
-                    <div className='col-md-4'>
-                        <h4 className='h4 text-center'>{order.orderStatus}</h4>
-                    </div>
-                    <div className='col-md-4 mt-4 '>
-                        <h4 className='h6 fw-bolder'>payment method : {order.paymentMethod}</h4>
-                        <h4 className='h6 fw-bolder'>is paid: {`${order.isPaid}`}</h4>
-                        <h4 className='h6 fw-bolder'>is delivered: {`${order.isDelivered}`}</h4>
-                        <h4 className='h6 fw-bolder'>total price: {order.totalPrice}</h4>
-                        <button className='btn bg-outline-main start-0'>more detailes</button>
-                    </div>
-                </div>
-                )}
-            </Link>
+                    )
+                </Link>
+            </>
+            }
         </div>
     </>
 }
